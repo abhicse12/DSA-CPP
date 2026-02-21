521cce72db232c439b0d05548bcad78a7d9c3d81
abhicse12 <pandeyabhisbek8@gmail.com>
2026-02-22 04:58:29 +0530

    stack question

---
+++ Stack/RPN.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+
+        for (const string& token : tokens) {
+            if (token == "+" || token == "-" ||
+                token == "*" || token == "/") {
+
+                int b = st.top(); st.pop();
+                int a = st.top(); st.pop();
+
+                if (token == "+") st.push(a + b);
+                else if (token == "-") st.push(a - b);
+                else if (token == "*") st.push(a * b);
+                else st.push(a / b);
+            }
+            else {
+                st.push(stoi(token));
+            }
+        }
+        return st.top();
+    }
+};
+
+int main() {
+    Solution s;
+    vector<string> tokens = {"2","1","+","3","*"};
+    cout << s.evalRPN(tokens);
+}
